c389571a7d9837b7bb577380470c8f1f5891c9b1
Fran <fma@ason.ch>
2023-11-10 13:51:05 +0100

    kn04

---
+++ KN04/b/x.php
@@ -0,0 +1,13 @@
+	<?php $ihrname = "Fran Matkovic"; ?>
+<html>
+	<head>
+	<title><?php echo($ihrname) ?></title>
+	</head>
+	<body>
+Ziege
+	<br />
+	<img src="https://frans3.s3.us-east-1.amazonaws.com/neom-dfglhJbc4Uc-unsplash.jpg?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEFsaDGV1LWNlbnRyYWwtMSJIMEYCIQDAq7QWYgKotTogFlPVfotXmhIounqcATW6SGGts23pagIhAO%2BYzVWmL9uwP5HseqacAm6rE1IRr8pvYg%2B8t1tlGya2KoIDCJT%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEQABoMNjQ5NjI1MDgwNDkwIgxmPgsJgEuOTR9%2B7CMq1gJ5t%2FO%2FZzAeEJ%2FAR3%2BtDnyvhOTWyGj49zim%2BYXoJcnyJUsOqC3dzvqQaQCRFgrR6QcQaTxPXBIVe49HHm3uvkOPqY52vL1kxWlNFofugg5m9qJOVPieoqgL4du31tMuAn%2B2kJil0k15SdASGfZga%2BZ0ZoUPXQxtm5IcMCj3ZUSnbmDpE%2BQG7PhhP8VeEo7cyN0s6v4EreEAMMxikydph78fIdcm0fVCOt39eRitopRXWe%2BJ7GqdGF4eBhL%2FiONW%2BUSlbuJEnQwBuXgmcTlINpVVvX0%2BtPAwRjt8Rwb9UAWWW7b3QOwrQjIOFtGjAo23XkbI3%2FL0p5HJHDAN39QL2paCJjSuWP9krQM0Fkxq%2F0ekwPW7BGo9DFgVJLTPYPLsMgaM0zfWmznsaeu%2BsoCE8FZU7NinS18saf7jNyrzdqnVGzjAH%2BBiwx3t6rd%2BPce25rXtAWpcn20w9eWkqgY6hgKgSXgSuBU0Q%2BJyPHRI9l%2FPJtGwZzcY5p4mT9qNfjGJalIGwlQjCHzJq025nW974MIZBvgM%2BAbiQy1W6g174GMPq8qMfh2CooRX9eebVYJ2YIvFo%2FkYuT%2FglatFG%2B3bgcVqCg%2BKOQmw1NqsIpHsaWShOX7Q%2FZCIzYcqR%2FBeuBcneI%2BYuKWUWFNN6S2yGjcM%2F0zzxE8xfv%2Bp9zSK1qgdZYIYRBM2J2Isemf%2B0yseO31GEY%2BfkiQPHYs7%2FsbnVgPJYQ0kj6P857qi3dxbSmnTV7qdaZ6ZMUdMeZzUgRw8aQiEG9xXgSTvHN4pF8Ujrl73PLap5pV8LUVMNjK8cc5i15UXFMPzuKxl&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20231106T185313Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIAZOQFMCKVE4LDD5UA%2F20231106%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=0883eedd5d676be54e5f7e8a0ad04cb25ad73cd49c736a9f9d8ffd77fad1fa6c" />
+	
+	Bild von unsplash
+	</body>
+</html>
---
+++ KN04/d/d.md
@@ -0,0 +1,36 @@
+**AWS S3 (Simple Storage Service):**
+
+- **Dauerhaftigkeit:** Ihre Daten sind immer sicher gespeichert.
+- **Leistungsanpassung:** Verschiedene Leistungsstufen stehen zur Verfügung.
+- **Anwendungsfälle:**
+  - Hosting von Websites
+  - Langfristige Datenspeicherung
+  - Datensicherung
+  - Datenlakes für flexible Datenanalyse (Intelligent-Tiering)
+  - Archivierung von Daten über lange Zeiträume (Glacier)
+- **Sicherheit:** Unterstützt Zugriffssteuerung und Verschlüsselung.
+- **Verfügbarkeit:** Weltweite Präsenz.
+
+**AWS EBS (Elastic Block Store):**
+
+- **Datenbeständigkeit:** Ihre Daten überstehen Ausfälle von Serverinstanzen.
+- **Leistungsvielfalt:** Unterschiedliche Typen, von herkömmlich bis Hochleistung.
+- **Einsatzszenarien:**
+  - Webserver (General Purpose SSD)
+  - Kleinere bis mittlere Datenbanken (General Purpose SSD)
+  - Leistungsstarke Datenbanken (Provisioned IOPS SSD)
+  - I/O-intensive Anwendungen (Provisioned IOPS SSD)
+  - Datenanalyse und Streaming (Durchsatzoptimiert HDD)
+- **Sicherheit:** Nutzt Verschlüsselung und ermöglicht präzise Zugriffssteuerung.
+- **Verfügbarkeit:** Auf Regionsebene beschränkt.
+
+**AWS EFS (Elastic File System):**
+
+- **Datenbeständigkeit:** Skalierbarkeit ohne Unterbrechungen.
+- **Leistungsflexibilität:** Unterschiedliche Leistungsmodi je nach Bedarf.
+- **Einsatzszenarien:**
+  - Webserver (General Purpose)
+  - Content-Management-Systeme (General Purpose)
+  - Anwendungen mit niedriger Latenz und hohem Durchsatz (Max I/O)
+- **Sicherheit:** Unterstützt Verschlüsselung und ermöglicht präzise Zugriffssteuerung.
+- **Verfügbarkeit:** Auf regionaler Ebene, mit mehreren Verfügbarkeitszonen.
